1abd687193daa4915d2b2f1bdd99abde86690fac
Josana-Kla <josana.0205@gmail.com>
2022-07-28 15:04:28 -0300

    feat: first page structure and style

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,5 +1,18 @@
 export default function Movies() {
     return (
-        <></>
+        <main>
+            <h2 className="flex-center">Selecione o filme</h2>
+            <div className="movies-list flex-center">
+                <div>
+                    <img src="../../assets/img/movie-2067.png" alt="" />
+                </div>
+                <div>
+                    <img src="../../assets/img/movie-2067.png" alt="" />
+                </div>
+                <div>
+                    <img src="../../assets/img/movie-2067.png" alt="" />
+                </div>
+            </div>
+        </main>
     )
 }
