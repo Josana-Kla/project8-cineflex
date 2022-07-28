a99bdb08f53ca8416c681a3f9e71bc217f99486e
Josana-Kla <josana.0205@gmail.com>
2022-07-28 15:42:03 -0300

    feat: componetized header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import Header from "./components/header/Header";
 import DateAndHour from "./pages/DateHourMovie";
 import Movies from "./pages/Movies";
 
 export default function App() {
     return (
         <>
-            <header className="flex-center">
-                <h1>CINEFLEX</h1>
-            </header>
+            <Header />
             
            {/*  <Movies /> */}
             <DateAndHour />
---
+++ src/components/header/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex-center">
+            <h1>CINEFLEX</h1>
+        </header>
+    )
+}
