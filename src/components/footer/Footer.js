5e96d7ebe4898840c166c2830e103461e64818b7
Josana-Kla <josana.0205@gmail.com>
2022-07-28 15:44:05 -0300

    feat: componentized footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,13 @@
 export default function Footer() {
     return (
-
+        <footer>
+            <div className="movie-chosen movie-card">
+                <img className="movie-card-img" src="../../assets/img/movie-2067.png" alt="" />
+            </div>
+            <div>
+                <h3>Enola Holmes</h3>
+                <h3>Quinta-feira - 15:00</h3>
+            </div>
+        </footer>
     )
 }
--- src/pages/DateHourMovie.js
+++ src/pages/DateHourMovie.js
@@ -1,3 +1,5 @@
+import Footer from "../components/footer/Footer";
+
 export default function DateAndHour() {
     return (
         <>
@@ -18,16 +20,8 @@ export default function DateAndHour() {
                     </div>
                 </div>
             </main>
-            
-            <footer>
-                <div className="movie-chosen movie-card">
-                    <img className="movie-card-img" src="../../assets/img/movie-2067.png" alt="" />
-                </div>
-                <div>
-                    <h3>Enola Holmes</h3>
-                    <h3>Quinta-feira - 15:00</h3>
-                </div>
-            </footer>
+
+            <Footer />
         </>
     )
 }
