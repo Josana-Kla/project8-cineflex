a097a7827ea2787c51b486968c3dc32b08592b29
Josana-Kla <josana.0205@gmail.com>
2022-07-29 06:59:20 -0300

    feat: instaled and used react router dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Header from "./components/header/Header";
 import DateAndHour from "./pages/DateHourMovie";
 import SeatsMovie from "./pages/SeatsMovie";
@@ -5,12 +7,14 @@ import Movies from "./pages/Movies";
 
 export default function App() {
     return (
-        <>
+        <BrowserRouter>
             <Header />
-            
-           {/*  <Movies /> */}
-            {/* <DateAndHour /> */}
-            <SeatsMovie />
-        </>
+            <Routes>
+                <Route path="/" element={<Movies />} /> 
+                <Route path="/filme" element={<DateAndHour />} />
+                <Route path="/sessao" element={<SeatsMovie />} />
+                <Route path="/sucesso" element={<></>} />
+            </Routes>
+        </BrowserRouter>
     )
 }
