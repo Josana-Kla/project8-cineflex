fa1ff97f6cc336fb81a40feeb4981a9889bd04e8
Josana-Kla <josana.0205@gmail.com>
2022-07-29 07:33:11 -0300

    feat: get movies with axios and used useeffect and usestate

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,18 +1,33 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 export default function Movies() {
+    const [ cards, setCards ] = useState([]);
+
+    useEffect(() => {
+        const request = axios.get("https://mock-api.driven.com.br/api/v7/cineflex/movies");
+
+        request.then(response => {
+            setCards(response.data);
+        });
+    }, []);
+
     return (
         <main>
             <h2 className="flex-center">Selecione o filme</h2>
             <div className="movies-list flex-center">
-                <div className="movie-card">
-                    <img className="movie-card-img" src="../../assets/img/movie-2067.png" alt="" />
-                </div>
-                <div>
-                    <img src="../../assets/img/movie-2067.png" alt="" />
-                </div>
-                <div>
-                    <img src="../../assets/img/movie-2067.png" alt="" />
-                </div>
+                {cards.map((card, index)=> 
+                    <Cards key={card.id} image={card.posterURL} />
+                )}
             </div>
         </main>
+    )
+}
+
+function Cards ( { image } ) {
+    return (
+        <div className="movie-card">
+            <img className="movie-card-img" src={image} alt="" />
+        </div>
     )
 }
