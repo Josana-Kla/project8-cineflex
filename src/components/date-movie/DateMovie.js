dba17f00831347945f3ef3ef33c05d73bc8e6bd1
Josana-Kla <josana.0205@gmail.com>
2022-07-30 12:06:07 -0300

    feat: implemented map inside hours session

--- src/components/date-movie/DateMovie.js
+++ src/components/date-movie/DateMovie.js
@@ -1,11 +1,23 @@
-export default function DateMovie( { day, date } ) {
+export default function DateMovie( { day, date, hour } ) {
+    
+   /*  for(let i = 0; i < hour; i++) {
+        console.log(i);
+    } */
+    
+    console.log(hour)
     return (
         <>
             <h3>{day} - {date}</h3>
             <div className="hours">
-                <button>15:00</button>
+                <HourButton>{hour.name}</HourButton>
                 <button>19:00</button>
             </div>
         </>
+    )
+}
+
+function HourButton(props) {
+    return (
+        <button>{props.children}</button>
     )
 }
--- src/pages/DateHourMovie.js
+++ src/pages/DateHourMovie.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-import DateMovie from "../components/date-movie/DateMovie";
-import Footer from "../components/footer/Footer";
+/* import DateMovie from "../components/date-movie/DateMovie";
+ */import Footer from "../components/footer/Footer";
 
 export default function DateAndHour() {
     const [ sessions, setSessions ] = useState([]);
@@ -13,26 +13,34 @@ export default function DateAndHour() {
         const promise = axios.get(`https://mock-api.driven.com.br/api/v7/cineflex/movies/${idFilme}/showtimes`);
 
         promise.then((response) => {
-            setSessions(response.data.days)
+            setSessions(response.data.days);
         });
         promise.catch(() => console.log("error"));
 
     }, []);
 
+   
 
     return (
         <>
             <main>
                 <h2 className="flex-center">Selecione o horário</h2>
-                <div className="date">
-                    {sessions.map((session, index) => (
-                        <DateMovie day={session.weekday} date={session.date} key={index} />
-                    ))}
-                </div>
+                {sessions.map((session, index) => (
+                    <div className="date" key={index}>
+                        <h3>{session.weekday} - {session.date}</h3>
+                        <div className="hours">
+                            {session.showtimes.map((value, index) => (
+                                <button key={index}>{value.name}</button>
+                            ))}
+                        </div>
+                    </div>
+                ))}
             </main>
 
             <Footer />
         </>
     )
 }
+
+{/* <DateMovie day={session.weekday} date={session.date} hour={session.showtimes[eachHour]} key={index} /> */}
 
