36a5c39fae425e9c74160f12e4e2e68ffbbdb4f7
Josana-Kla <josana.0205@gmail.com>
2022-07-31 21:22:08 -0300

    style: added font Roboto

--- src/pages/DateHourMovie.js
+++ src/pages/DateHourMovie.js
@@ -42,8 +42,7 @@ export default function DateAndHour() {
                 </div>
             </main>
 
-            <Footer image={chosenMovie.posterURL} title={chosenMovie.title} >
-            </Footer>
+            <Footer image={chosenMovie.posterURL} title={chosenMovie.title}></Footer>
         </>
     )
 }
